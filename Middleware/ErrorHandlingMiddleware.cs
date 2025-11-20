f5208b72c407b8db2d12e566653163af6b590790
LL-LQ-84i <155986030+orangejuiceplz@users.noreply.github.com>
2025-11-20 16:39:59 -0500

    add ErrorHandlingMiddleware to manage exceptions and return standardized error responses

---
+++ Middleware/ErrorHandlingMiddleware.cs
@@ -0,0 +1,56 @@
+using System.Net;
+using System.Text.Json;
+
+namespace EventiveAPI.CSharp.Middleware;
+
+public class ErrorHandlingMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger<ErrorHandlingMiddleware> _logger;
+
+    public ErrorHandlingMiddleware(RequestDelegate next, ILogger<ErrorHandlingMiddleware> logger)
+    {
+        _next = next;
+        _logger = logger;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Unhandled exception occurred");
+            await HandleExceptionAsync(context, ex);
+        }
+    }
+
+    private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+    {
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+        var response = new
+        {
+            success = false,
+            error = new
+            {
+                message = "An internal server error occurred",
+                details = exception.Message
+            }
+        };
+
+        var json = JsonSerializer.Serialize(response);
+        return context.Response.WriteAsync(json);
+    }
+}
+
+public static class ErrorHandlingMiddlewareExtensions
+{
+    public static IApplicationBuilder UseErrorHandling(this IApplicationBuilder builder)
+    {
+        return builder.UseMiddleware<ErrorHandlingMiddleware>();
+    }
+}
